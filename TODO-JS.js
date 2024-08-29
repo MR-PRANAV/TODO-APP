267769e7c5087f9f63e93470cfafbaa8ca57ec4d
MR-PRANAV <pranavpatil020389@gmail.com>
2024-08-29 12:00:07 +0530

    TODO COMMIT 1

---
+++ TODO-JS.js
@@ -0,0 +1,37 @@
+//  @_MR_PRANAV____
+//  A SIMPLE TODO APP USING JAVA-SCRIPT WOTHOUT USING DOM CONCEPT 
+
+let arr = [];
+let req =  prompt("PLEASE ENTER A REQUEST")
+
+while(true){
+    if(req == "4"){
+        console.log("QUITING FROM APP");
+        arr = [];
+        break;
+    }
+    
+    if (req == "2") {
+        console.log("-----------");
+        console.log("YOUR TASK :");
+        for(let i=0 ; i<=arr.length-1 ;i++){
+            console.log(`Task No ${i} : ${arr[i]}`);
+        }
+        console.log("-----------");
+    }
+    else if(req == "1"){
+        let t = prompt("ENTER A TASK");
+        arr.push(t);
+        console.log("TASK ADDED");
+    }
+    else if(req == "3"){
+        let del = prompt("ENTER A NUMBER OF TASK YOU WANT TO DELETE");
+        arr.splice(del ,1 );
+        console.log(`TASK DELETED`)
+    }else{
+        alert("WRONG INPUT PLEASE TRY AGAIN")
+    }
+    req =  prompt("PLEASE ENTER A NEXT REQUEST")
+}
+
+
